b9c5476e2bdb71b0383be082c32d6c7f4b939324
MisteRy Tech <47310669+misteryTech@users.noreply.github.com>
2026-09-19 00:56:02 +0800

    PUSH FIRST REQUEST
    PUSH FIRST REQUEST

--- cleveroaks/settings.py
+++ cleveroaks/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-*6x@w^e)nowwdsg%t$j*c5vkcbz5g63j$thyj41#z!-6vrw#vv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "192.168.0.103",
+    "localhost",
+    "127.0.0.1",
+]
+
 
 
 # Application definition
--- users/views.py
+++ users/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 
 def register(request):
-    return render(request, 'index.html')
+    return render(request, 'registration.html')
 # Create your views here.
+
+def login_user(request):
+    return render(request, 'login.html')
